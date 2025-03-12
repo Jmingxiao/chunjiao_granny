11e71c5ecd83ba2cf598dacf5753dfe2e15fbb1b
Jmingxiao <Mingxiao_0719@outlook.com>
2025-03-12 22:07:55 +0800

    [SRM250301]: 添加工具代码以及第三方手势库
    Description:
    Type: BUG REFACTOR FEATURE

---
+++ Assets/Scripts/Utils/Event/EventDefine.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ACG
+{
+    public static class EvenDefine
+    {
+        //Gesture Event Start from 100
+        public const int SingleFingerSingleTap = 100;
+        public const int LongPress = 101;
+
+        //For Debug
+        public const int TestBroadcast = 1000;
+    }
+}
